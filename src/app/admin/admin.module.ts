8a59420df8169bbbb00616a5d6d69c604aacd4f4
Ryifki Yidan <ryifkiyidan@gmail.com>
2020-10-15 03:09:08 +0700

    Hammerjs import LongPressModule

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { AdminPageRoutingModule } from './admin-routing.module';
 
 import { AdminPage } from './admin.page';
+import {LongPressModule} from 'ionic-long-press';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    AdminPageRoutingModule
+    AdminPageRoutingModule,
+    LongPressModule
   ],
   declarations: [AdminPage]
 })
